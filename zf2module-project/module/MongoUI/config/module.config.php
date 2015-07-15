cd92361c7c5e1b7338dbbd77b469d011ace1bc44
Florian <laforest.florian@gmail.com>
2015-07-15 02:12:07 +0200

    Ajout d'un layout (menu, CSS) Suppression du CSS "en dur" dans les fichiers

--- zf2module-project/module/MongoUI/config/module.config.php
+++ zf2module-project/module/MongoUI/config/module.config.php
@@ -37,6 +37,9 @@ return [
 		],
 	],
 	 'view_manager' => [
+	 	'template_map' => [
+	 		'layout/layout' => __DIR__ . '/../view/layout/layout.phtml'
+	 	],
         'template_path_stack' => [
             __DIR__ . '/../view',
         ],
